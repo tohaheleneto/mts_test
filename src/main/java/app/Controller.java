22dd92a5f0f2d709020cea01f360c4f468c45fcb
tohaheleneto <anton_mamont@bk.ru>
2019-05-03 02:42:30 +0300

    critical api bug fixed

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -1,59 +1,44 @@
 package app;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 
 @RestController
-@EnableAsync
 public class Controller
 {
-
     @Autowired
     private TaskRepository taskRepository;
 
     @PostMapping("/task")
-    public ResponseEntity<?> doSmth() {
-        Task task = new Task();
-        taskRepository.save(task);
+    public ResponseEntity<?> create() {
+        Task task = taskRepository.save(new Task());
         new Thread(() -> {
-            Task us = taskRepository.findById(task.id).get();
-            us.setStatus("running");
-            us.setTimestamp(LocalDateTime.now());
-            taskRepository.save(us);
+            task.setStatus("running");
+            task.setTimestamp(LocalDateTime.now());
+            taskRepository.save(task);
             try {
                 TimeUnit.MINUTES.sleep(2);
             }catch (Exception e)
             {
                 e.printStackTrace();
             }
-            us.setTimestamp(LocalDateTime.now());
-            us.setStatus("finished");
-            taskRepository.save(us);
+            task.setTimestamp(LocalDateTime.now());
+            task.setStatus("finished");
+            taskRepository.save(task);
         }).start();
         return new ResponseEntity<>(task.id,HttpStatus.valueOf(202));
     }
 
-    @GetMapping("/task")
-    public ResponseEntity<?> done(@RequestParam("id") String id) {
-
+    @GetMapping("/task/{id}")
+    public ResponseEntity<?> get(@PathVariable("id") String id) {
         try{
             UUID uuid = UUID.fromString(id);
             Optional<Task> task = taskRepository.findById(uuid);
@@ -65,42 +50,8 @@ public class Controller
         }
     }
     
-    /*
-        Method that start threads to managing of unfinished task if server restarted
-    */
-    @EventListener(ApplicationReadyEvent.class)
-    public void doSomethingAfterStartup() {
-        ArrayList<Task> tasksNeedToChangeStatus = new ArrayList<>();
 
-        for (Task task : taskRepository.findAll())
-        {
-            if (task.status.equals("running")) {
-                if (task.getTimestamp().until(LocalDateTime.now(), ChronoUnit.MINUTES) >= 2) {
-                    task.setTimestamp(task.getTimestamp().plusMinutes(2));
-                    task.setStatus("finished");
-                    taskRepository.save(task);
-                }
-                else {
-                    tasksNeedToChangeStatus.add(task);
-                }
-            }
-        }
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(tasksNeedToChangeStatus.size());
-        for (Task task : tasksNeedToChangeStatus)
-        {
-            service.submit(() -> {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(LocalDateTime.now().until(task.timestamp.plusMinutes(2), ChronoUnit.MILLIS));
-                }catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-                task.setTimestamp(task.getTimestamp().plusMinutes(2));
-                task.setStatus("finished");
-                taskRepository.save(task);
-            });
-        }
-    }
+
 
     @GetMapping("/")
     public String testHeroku() {
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,19 @@
 package app;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 @EnableAsync
@@ -11,5 +21,45 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    /*
+        Method that start threads to managing of unfinished task if server restarted
+    */
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        ArrayList<Task> tasksNeedToChangeStatus = new ArrayList<>();
+
+        for (Task task : taskRepository.findAll())
+        {
+            if (task.status.equals("running")) {
+                if (task.getTimestamp().until(LocalDateTime.now(), ChronoUnit.MINUTES) >= 2) {
+                    task.setTimestamp(task.getTimestamp().plusMinutes(2));
+                    task.setStatus("finished");
+                    taskRepository.save(task);
+                }
+                else {
+                    tasksNeedToChangeStatus.add(task);
+                }
+            }
+        }
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(tasksNeedToChangeStatus.size());
+        for (Task task : tasksNeedToChangeStatus)
+        {
+            service.submit(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(LocalDateTime.now().until(task.timestamp.plusMinutes(2), ChronoUnit.MILLIS));
+                }catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                task.setTimestamp(task.getTimestamp().plusMinutes(2));
+                task.setStatus("finished");
+                taskRepository.save(task);
+            });
+        }
     }
 }
